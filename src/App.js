f78c286c8cb9bd800577da9d6175e0c08f632bdb
Kareem Grant <kareem@getuserwise.com>
2019-03-22 12:39:06 -0400

    add content

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
         <p>Hello from Kareem Grant</p>
-        
+
         <img src="https://images.unsplash.com/photo-1515619363794-e826f7de3487?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" />
       </div>
+      <p>Rainforest pic</p>
     );
   }
 }
