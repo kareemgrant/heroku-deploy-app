86a47d1ad1b737d6e95472df4121bf27aa93e4a2
Kareem Grant <kareem@getuserwise.com>
2019-03-22 12:22:53 -0400

    Add rainforest image

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <p>Hello from Kareem Grant</p>
+        
+        <img src="https://images.unsplash.com/photo-1515619363794-e826f7de3487?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" />
       </div>
     );
   }
